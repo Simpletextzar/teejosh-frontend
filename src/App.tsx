5dc987a3a86f1e13f1955fa803e329df5ea78590
simpletextzar <simpletextzar@gmail.com>
2025-12-04 19:26:38 -0500

    import react quitado por no ser requerido

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Inventory from "./components/InventoryView";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
--- src/pages/Inventario.tsx
+++ src/pages/Inventario.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Inventario = () => {
   return <div>Inv</div>;
 };
